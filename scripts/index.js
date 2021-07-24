64e396fda9c37f789d57cf16ed0522501d5af5ea
SailajaRadjou <p.sailaja@hotmail.com>
2021-07-24 22:34:42 +0200

    afficher les images correctement dans le link tag

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,36 @@
+
 contentLoad();
+
 
 function contentLoad()
 {
     let url = 'http://localhost:3000/api/teddies';
-    fetch('http://localhost:3000/api/teddies').then((Response) => 
-        Response.json().then((data) => 
+    fetch('http://localhost:3000/api/teddies').then(function(Response){
+        return Response.json();
+    })
+    .then(function(data)
         {
-            console.log(data);
-            
-            for(let teddy of data)
+            const articles = data;
+            console.log(articles);
+            for(let teddy of articles)
             {
-                let productImageDiv = document.createElement("div");
+                const productImageDiv = document.createElement("div");
                 document.querySelector(".displaycontent").appendChild(productImageDiv);
                 productImageDiv.classList.add("productList");
-                
-                let productImageImg = document.createElement("img");
-                document.querySelector(".productList").appendChild(productImageImg);
-                productImageImg.src = teddy.imageUrl;
+
+                const productLink = document.createElement("a");
+                productImageDiv.appendChild(productLink);
+                productLink.className = 'product_link';
+                productLink.setAttribute('title', "L'ourson " + teddy.name + " vous attend !");
+
+                const productImageImg = document.createElement("img");
+                productLink.appendChild(productImageImg);
+                productImageImg.classList.add("product_image");
+                productImageImg.setAttribute('src', teddy.imageUrl);
+                productImageImg.setAttribute('alt', 'Ours en peluche ' + teddy.name);
+                productImageImg.setAttribute('title', 'Ours en peluche ' + teddy.name);
             }
             
         })
-    ).catch(err => console.log('error : '+ err));
+    .catch(err => console.log('error : '+ err));
 }
