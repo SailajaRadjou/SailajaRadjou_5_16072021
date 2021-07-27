33a56c80de851273990df949b44a9fa64fcab1c6
SailajaRadjou <p.sailaja@hotmail.com>
2021-07-27 16:00:26 +0200

    ajouter la dropdown list box with label

--- scripts/product.js
+++ scripts/product.js
@@ -14,11 +14,11 @@ function getProduct()
       .then(function(receivedData)
       {
         const products = receivedData;
-        console.log(products);
+        console.log(products.colors);
 
         const productImageDiv = document.createElement("div");
         document.querySelector(".product_description").appendChild(productImageDiv);
-        productImageDiv.classList.add("product_dis");
+        productImageDiv.classList.add("product_display_container");
 
         const productImageImg = document.createElement("img");
         productImageDiv.appendChild(productImageImg);
@@ -26,6 +26,35 @@ function getProduct()
         productImageImg.setAttribute('src', products.imageUrl);
         productImageImg.setAttribute('alt', 'Ours en peluche ' + products.name);
         productImageImg.setAttribute('title', 'Ours en peluche ' + products.name);
+
+        const productDetailDiv = document.createElement("div");
+        document.querySelector(".product_description").appendChild(productDetailDiv);
+        productDetailDiv.classList.add("product_details_display");
+
+        const productTitle = document.createElement("h3");
+        productDetailDiv.appendChild(productTitle);
+        productTitle.classList.add("product_title");
+        productTitle.innerHTML = products.name;
+
+        const productLabelDescription = document.createElement("label");
+        //productLabelDescription.setAttribute("for",description);
+        productLabelDescription.innerHTML = "Description :  " + products.description;
+        productDetailDiv.appendChild(productLabelDescription);
+
+        const productLabelColor = document.createElement("label");
+        productLabelColor.htmlFor = "colorlist";
+        productLabelColor.innerHTML = "Color :  ";
+        productDetailDiv.appendChild(productLabelColor);
+
+       const colors = products.colors;
+       const colorList = document.createElement("select");
+       colorList.setAttribute("name","colorlist");
+       for (let i = 0; i < colors.length; i++) {
+            const selectOption = document.createElement('option');
+            productLabelColor.appendChild(colorList);
+            colorList.textContent += colors[i];
+            colorList.setAttribute("value", colors[i]);
+        }
       })
 
 }      
