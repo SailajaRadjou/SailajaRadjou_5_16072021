f2e1486c8b7bce71945cd0dce15620381ef668e7
SailajaRadjou <p.sailaja@hotmail.com>
2021-07-27 14:18:58 +0200

    ajouter la mise en page de produits

--- scripts/product.js
+++ scripts/product.js
@@ -3,7 +3,7 @@ const urlQuery = window.location.search;
 const urlParams = new URLSearchParams(urlQuery);
 const id = urlParams.get('id');
 console.log(id);
-function productLoad()
+function getProduct()
 {
     // On récupère uniquement le produit via le paramètre dans la requête
     fetch('http://localhost:3000/api/teddies/' + id)
@@ -16,8 +16,18 @@ function productLoad()
         const products = receivedData;
         console.log(products);
 
+        const productImageDiv = document.createElement("div");
+        document.querySelector(".product_description").appendChild(productImageDiv);
+        productImageDiv.classList.add("product_dis");
+
+        const productImageImg = document.createElement("img");
+        productImageDiv.appendChild(productImageImg);
+        productImageImg.classList.add("product_image");
+        productImageImg.setAttribute('src', products.imageUrl);
+        productImageImg.setAttribute('alt', 'Ours en peluche ' + products.name);
+        productImageImg.setAttribute('title', 'Ours en peluche ' + products.name);
       })
 
 }      
 
-    productLoad();
+getProduct();
