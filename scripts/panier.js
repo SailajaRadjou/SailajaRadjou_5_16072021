bfd14b717bd41a5c8244654f35085313142f3dbe
SailajaRadjou <p.sailaja@hotmail.com>
2021-07-29 15:02:03 +0200

    ajouter le bouter pour vider le panier dans le panier.js

--- scripts/panier.js
+++ scripts/panier.js
@@ -1,3 +1,35 @@
 let selectedProducts = JSON.parse(localStorage.getItem('newProduct'));
 console.log(selectedProducts);
+const cartMain = document.getElementById('cart_page');
 
+const deleteContent = document.getElementById('delete');
+    
+if(selectedProducts == null){
+    // si le panier est vide 
+    
+    document.getElementById("cart_container").style.display = "none";
+    const cartEmptyDiv = document.createElement('div');
+    cartMain.appendChild(cartEmptyDiv);
+    cartEmptyDiv.className = 'cart_content';
+    const emptyCart = document.createElement('p');
+    cartEmptyDiv.appendChild(emptyCart);
+    emptyCart.className = "cart_empty";
+    emptyCart.textContent = "Votre panier est vide !"
+} //else {
+
+//eventListener pour vider le panier
+deleteContent.addEventListener("click", function (event) {
+    localStorage.removeItem('newProduct');
+
+    document.getElementById("cart_container").style.display = "none";
+
+    const cartEmptyDiv = document.createElement('div');
+    cartMain.appendChild(cartEmptyDiv);
+    cartEmptyDiv.className = 'cart_content';
+
+    const emptyCart = document.createElement('p');
+    cartEmptyDiv.appendChild(emptyCart);
+    emptyCart.className = "cart_empty";
+    emptyCart.textContent = "Votre panier est vide !"
+})
+    
